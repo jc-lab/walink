499a7f30bdd8aa3050a3bc7a3f2de6868d44606e
Joseph Lee <joseph@jc-lab.net>
2025-12-29 02:30:33 +0900

    feat(go): msgpack

--- go/wlvalue/msgpack.go
+++ go/wlvalue/msgpack.go
@@ -6,11 +6,12 @@ import (
 	"github.com/vmihailenco/msgpack/v5"
 )
 
-type MsgpackOption func(d *msgpack.Decoder)
+type MsgpackOption func(d *msgpack.Decoder) error
 
 func MsgpackTag(tag string) MsgpackOption {
-	return func(d *msgpack.Decoder) {
+	return func(d *msgpack.Decoder) error {
 		d.SetCustomStructTag(tag)
+		return nil
 	}
 }
 
@@ -18,7 +19,12 @@ func DecodeMsgpack(v Value, allowFree bool, out interface{}, options ...MsgpackO
 	raw := ToMsgpackBytes(v, allowFree)
 	d := msgpack.NewDecoder(bytes.NewReader(raw))
 	for _, option := range options {
-		option(d)
+		if err := option(d); err != nil {
+			return err
+		}
+	}
+	if out == nil {
+		return nil
 	}
 	return d.Decode(out)
 }
